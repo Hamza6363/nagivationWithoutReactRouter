926d883cb9d57daaf9e9fb5451f83ce344641717
Hamza6363 <ameerhamzay63@gmail.com>
2023-04-30 23:19:15 +0500

    Complete single page stateManagment

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,23 +1,38 @@
 import React from 'react'
-
+import { useState } from 'react'
 export default function Contact() {
   
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("")
+  
+
+  const submitHandler = (e) =>{
+    e.preventDefault();
+
+    const data = {
+      name: name,
+      email: email,
+      gender: gender
+    }
+
+    console.log(data);
+  }
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='form-group'>
         <label htmlFor="">Name</label>
-        <input type="text" className='form-control'/>
+        <input type="text" className='form-control' onChange={(e)=>setName(e.target.value)}/>
       </div>
       <div className='form-group'>
         <label htmlFor="">Email</label>
-        <input type="email" className='form-control'/>
+        <input type="email" className='form-control' onChange={(e)=>setEmail(e.target.value)}/>
       </div>
       <div className='form-group'>
         <label>Gender</label>
-
-        <input type="checkbox" name="" id="male" />
+        <input type="checkbox" name="" id="male" onChange={(e)=> setGender(e.target.id) } />
         <label htmlFor="male">Male</label>
-        <input type="checkbox" name="" id="female" />
+        <input type="checkbox" name="" id="female" onChange={(e)=> setGender(e.target.id)} />
         <label htmlFor="female">female</label>
       </div>
       <div className='form-group'>
