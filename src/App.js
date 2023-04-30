b56467a52e0b1f29729837c81d4d7c5b7bb0a137
Hamza6363 <ameerhamzay63@gmail.com>
2023-04-30 17:03:17 +0500

    add whole navigation process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Service from './pages/Service/Service';
+import Contact from './pages/Contact/Contact';
+
+const HomePage = () =>{
+  if(window.location.pathname === "/"){
+    return <Home />
+  }
+}
+const AboutPage = () =>{
+  if(window.location.pathname === "/about"){
+    return <About />
+  }
+}
+const ServicePage = () =>{
+  if(window.location.pathname === "/service"){
+    return <Service />
+  }
+}
+const ContactPage = () =>{
+  if(window.location.pathname === "/contact"){
+    return <Contact />
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      
+      {HomePage()}
+      {AboutPage()}
+      {ServicePage()}
+      {ContactPage()}
     </div>
   );
 }
