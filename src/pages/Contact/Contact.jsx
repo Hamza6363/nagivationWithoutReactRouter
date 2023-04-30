f3a16bc987208c72d2d738c295d22a0804856775
Hamza6363 <ameerhamzay63@gmail.com>
2023-04-30 17:44:40 +0500

    make a contact form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 
 export default function Contact() {
+  
   return (
-    <div>Contact</div>
+    <form>
+      <div className='form-group'>
+        <label htmlFor="">Name</label>
+        <input type="text" className='form-control'/>
+      </div>
+      <div className='form-group'>
+        <label htmlFor="">Email</label>
+        <input type="email" className='form-control'/>
+      </div>
+      <div className='form-group'>
+        <label>Gender</label>
+
+        <input type="checkbox" name="" id="male" />
+        <label htmlFor="male">Male</label>
+        <input type="checkbox" name="" id="female" />
+        <label htmlFor="female">female</label>
+      </div>
+      <div className='form-group'>
+        <button>Submit</button>
+      </div>
+    </form>
   )
 }
