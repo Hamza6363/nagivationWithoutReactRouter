b52992a03dfa6fc029cea74776025a7915d904e6
Hamza6363 <ameerhamzay63@gmail.com>
2023-05-02 01:23:50 +0500

    statemanagment by useReducer

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { useState } from 'react'
+
+
+const formReducer = (state, action) =>{
+  console.log(state, action);
+  if(action.type === 'NAME_FIELD'){
+    state.name = action.val;
+    return state;
+  }
+  if(action.type === 'EMAIL_FIELD'){
+    state.email = action.val;
+    return state;
+  }
+  if(action.type === 'MALE_CHECKBOX'){
+    state.gender = action.val;
+    return state;
+  }
+  return state;
+}
+
 export default function Contact(props) {
+  const formInitalState = {
+    name: "",
+    email: "",
+    gender: ""
+  }
   
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [gender, setGender] = useState("")
-  
+  const [formState, dispatch]= useReducer(formReducer, formInitalState);
 
   const submitHandler = (e) =>{
     e.preventDefault();
 
     const data = {
-      name: name,
-      email: email,
-      gender: gender
+      name: formState.name,
+      email: formState.email,
+      gender: formState.gender
     }
     props.formSubmitData(data);
   }
@@ -21,17 +42,17 @@ export default function Contact(props) {
     <form onSubmit={submitHandler}>
       <div className='form-group'>
         <label htmlFor="">Name</label>
-        <input type="text" className='form-control' onChange={(e)=>setName(e.target.value)}/>
+        <input type="text" className='form-control' onChange={(e)=>dispatch({type: 'NAME_FIELD', val: e.target.value})}/>
       </div>
       <div className='form-group'>
         <label htmlFor="">Email</label>
-        <input type="email" className='form-control' onChange={(e)=>setEmail(e.target.value)}/>
+        <input type="email" className='form-control' onChange={(e)=>dispatch({type: 'EMAIL_FIELD', val: e.target.value})}/>
       </div>
       <div className='form-group'>
         <label>Gender</label>
-        <input type="checkbox" name="" id="male" onChange={(e)=> setGender(e.target.id) } />
+        <input type="checkbox" name="" id="male" onChange={(e)=>dispatch({type: 'MALE_CHECKBOX',  val: e.target.id})} />
         <label htmlFor="male">Male</label>
-        <input type="checkbox" name="" id="female" onChange={(e)=> setGender(e.target.id)} />
+        <input type="checkbox" name="" id="female" onChange={()=>dispatch({type: 'FEMALE_CHECKBOX'})} />
         <label htmlFor="female">female</label>
       </div>
       <div className='form-group'>
