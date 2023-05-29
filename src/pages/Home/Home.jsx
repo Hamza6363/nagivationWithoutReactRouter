d4782b95c45982e0f0d85afa1a18c4df6d7af73c
Hamza6363 <ameerhamzay63@gmail.com>
2023-05-30 01:32:12 +0500

    usage of useRef hook

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 
 export default function Home() {
+  
+  const inputElement = useRef();
+
+
   return (
-    <div>Home</div>
+    <div>Home
+      <form>
+        <label htmlFor=""></label>
+        <input ref={inputElement} type="text" onChange={(e)=>{console.log(inputElement)}}/>
+      </form>
+    </div>
   )
 }
